fbac9b03cf82825de87d25f19e9d7cd743aa0cd8
Shivam Bansal <shivambansal157@gmail.com>
2020-09-13 10:09:56 +0530

    add index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+function show_login_form(){
+    document.getElementById('login_form').style.display = "block";
+    document.getElementById('signup-form').style.display = "none";
+  	}
+
+function show_signup_form(){
+    document.getElementById('signup-form').style.display = "block";
+    document.getElementById('login_form').style.display = "none";
+  	}
+      
